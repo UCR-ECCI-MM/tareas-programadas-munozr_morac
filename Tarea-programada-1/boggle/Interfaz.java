f64d4bf04505fcba1c0313a7f99de63a3ee61a95
JosePabloMoraC <84686084+JosePabloMoraC@users.noreply.github.com>
2021-09-19 21:23:21 -0600

    Creación clase interfaz
    Se crea la clase interfaz y se añaden sus atributos.

---
+++ Tarea-programada-1/boggle/Interfaz.java
@@ -0,0 +1,35 @@
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+/**
+ * Write a description of class Interfaz here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Interfaz extends JOptionPane
+{
+    // instance variables - replace the example below with your own
+    private final String TITULO;
+    private final ImageIcon IMAGEN;
+
+    /**
+     * Constructor for objects of class Interfaz
+     */
+    public Interfaz()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
