2c9660e33ce73d2f253ea1571bff4b8d43cae899
JosePabloMoraC <84686084+JosePabloMoraC@users.noreply.github.com>
2021-09-20 19:36:52 -0600

    Constructor Clase Dados

---
+++ Tarea-programada-1/boggle/Dado.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList; 
+/**
+ * Esta clase representa un dado. 
+ * 
+ * @author Kyara Muñoz
+ * @author Pablo Mora
+ */
+public class Dado
+{
+    // 
+    private ArrayList<String> carasDado;
+
+    /**
+     * Constructor de la clase Dados. 
+     */
+    public Dado()
+    {
+        carasDado = new ArrayList<String>();
+        
+    }
+
+}
