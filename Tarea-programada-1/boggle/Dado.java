4e32bc25354055793d07e31aed800098dd2555e7
JosePabloMoraC <84686084+JosePabloMoraC@users.noreply.github.com>
2021-09-22 19:55:41 -0600

    Constructor de la clase Dado

--- Tarea-programada-1/boggle/Dado.java
+++ Tarea-programada-1/boggle/Dado.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList; 
+import java.util.Random;
 /**
  * Esta clase representa un dado. 
  * 
@@ -8,24 +9,35 @@ import java.util.ArrayList;
 public class Dado
 {
     // 
-    private ArrayList<String> carasDado;
-    // private String caraVisible;
+    private char[] carasDado;
+    private char caraVisible;
+    private Random generador;
 
     /**
      * Constructor de la clase Dados. 
      */
     public Dado()
     {
+        generador = new Random();
+        carasDado = new char[6];
+        for(int indice = 0; indice<6; indice++) {
+            carasDado[indice] = (char)(65 + generador.nextInt(26));
+        }
+        caraVisible = carasDado[generador.nextInt(6)];
         //Atributos de la clase.
         //Contiene las 6 caras del dado. 
-        carasDado = new ArrayList<String>(); //Inicializar el arreglo. // Asignar aleatoriamente las caras //¿Cambiar a char?
+        //carasDado = new ArrayList<String>(); //Inicializar el arreglo. // Asignar aleatoriamente las caras //¿Cambiar a char?
         //caraVisible ->Forzosamente se le debe asignar un valor. La profe prefiere que sea una cara aleatoria por facilidad. 
-        
+        // ascii 65(A) a 90(Z)
     }
-
+    
     /**
-     * public void tirarDado() {
-     *     
-     }
+     * 
      */
+
+    
+    public void tirarDado() {
+       
+     }
+    
 }
