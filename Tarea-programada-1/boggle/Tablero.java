2b5b443cc30777c0c0b395da0e376acec358173d
KyaraMR <kyara.munoz@ucr.ac.cr>
2021-09-24 01:08:31 -0600

    clase Tablero completa

--- Tarea-programada-1/boggle/Tablero.java
+++ Tarea-programada-1/boggle/Tablero.java
@@ -1,6 +1,6 @@
-
+import java.util.Random;
 /**
- * Esta clase representa un dado. 
+ * Esta clase representa el tablero del juego con los dados 
  * 
  * @author Kyara Muñoz
  * @author Pablo Mora
@@ -8,18 +8,56 @@
  
 public class Tablero {       
     //Atributos de la clase
-    String [][] letrasTablero;    // debe ser de tipo dado.
+    private Dado[][] matrizDados;
+    private final int CANTIDAD_FILAS = 4;
+    private final int CANTIDAD_COLUMNAS = 4;
+    private Random generador;
     
-
-    
-    /*
-     * Constructor de la clase. 
+    /**
+     * Constructor para objetos de la clase Tablero
      */
-    public Tablero() {        
-        // Constructor de la clase        
-        letrasTablero = new String[4][4];  // inicializar  
-        // Crear 16 dados 
+    public Tablero() {  
+        matrizDados = new Dado[CANTIDAD_FILAS][CANTIDAD_COLUMNAS];
+        generador = new Random();
+        for (int fila = 0; fila < matrizDados.length; fila++) {
+            for (int columna = 0; columna < matrizDados[fila].length; columna++) {
+                matrizDados[fila][columna] = new Dado();
+            }
+        }
     }
     
-    //metodo revolver
+    public Dado[][] getMatrizDados() {
+        return matrizDados;
+    }
+    
+    public void setMatrizDados(Dado[][] matrizDados) {
+        this.matrizDados = matrizDados;
+    }
+    
+    public void agitarTablero() {
+        Dado temporal;
+        int filaAleatoria;
+        int columnaAleatoria;
+        for (int fila = 0; fila < matrizDados.length; fila++) {
+            for (int columna = 0; columna < matrizDados[fila].length; columna++) {
+                temporal = matrizDados[fila][columna];
+                filaAleatoria = generador.nextInt(CANTIDAD_FILAS);
+                columnaAleatoria = generador.nextInt(CANTIDAD_COLUMNAS);
+                matrizDados[fila][columna] = matrizDados[filaAleatoria][columnaAleatoria];
+                matrizDados[filaAleatoria][columnaAleatoria] = temporal;
+                matrizDados[fila][columna].tirarDado();
+            }
+        }
+    }
+    
+    public String toString() {
+        String hilera = "";
+        for (int fila = 0; fila < matrizDados.length; fila++) {
+            for (int columna = 0; columna < matrizDados[fila].length; columna++) {
+                hilera = hilera + matrizDados[fila][columna].getCaraVisible() + " ";
+            }
+            hilera = hilera + "\n";
+        }
+        return hilera;
+    }
 }
